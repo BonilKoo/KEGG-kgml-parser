3ccfa67b8e5f572bc45a5df15480ed1b31d8f8b4
Bonil Koo <68688071+BonilKoo@users.noreply.github.com>
2022-03-28 13:39:23 +0900

    Update kgml_parser.py

--- kgml_parser.py
+++ kgml_parser.py
@@ -80,7 +80,7 @@ def main():
     args = parse_args()
     
     # output directory
-    rmfile(args.output_dir)
+#     rmfile(args.output_dir)
     mkdir(args.output_dir)
     
     # load pathway list
